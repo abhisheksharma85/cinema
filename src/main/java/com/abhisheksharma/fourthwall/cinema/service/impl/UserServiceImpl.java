5b17419309347c60c2e741844a03f349f6512a20
Abhishek Sharma <abhi.scs@gmail.com>
2020-11-18 22:58:37 -0800

    User is active by default

--- src/main/java/com/abhisheksharma/fourthwall/cinema/service/impl/UserServiceImpl.java
+++ src/main/java/com/abhisheksharma/fourthwall/cinema/service/impl/UserServiceImpl.java
@@ -67,7 +67,7 @@ public class UserServiceImpl implements UserService {
         newUser.setImageUrl(userDTO.getImageUrl());
         newUser.setLangKey(userDTO.getLangKey());
         // new user is not active
-        newUser.setActivated(false);
+        newUser.setActivated(true);
         // new user gets registration key
         newUser.setActivationKey(RandomUtil.generateActivationKey());
         Set<Authority> authorities = new HashSet<>();
